d566a586b9b4b7cf87dddf9367c3dc870a341a41
david <1049698300@qq.com>
2019-08-05 14:44:40 +0800

    增加了反射 导入handler包下所有BaseHandler的子类

--- aicyber/com/data_channel/main.py
+++ aicyber/com/data_channel/main.py
@@ -2,6 +2,7 @@ from aicyber.com.data_channel.utils.Util import Utils
 from aicyber.com.data_channel.ModifyHandler import ModifyHandler
 from aicyber.com.data_channel.BaseHandler import BaseHandler
 import os
+import importlib
 
 class Main():
     def flow(self):
@@ -13,25 +14,36 @@ class Main():
         except Exception as e:
             print(e)
 
-    def loadUserHandler(self):
-        is_handler=lambda cls:isinstance(cls,type) and issubclass(cls,BaseHandler)
-
-    def getModules(self,package='.'):
-        modules=[]
-        files=os.listdir(package)
-        for file in files:
-            if not file.startswith('__'):
-                name,ext=os.path.splitext(file)
-                if ext:
-                    if ext=='.py':
-                        modules.append(package.replace('/','.'))
-
-
-
+    def loadUserHandler(self,package='./handler'):
+        is_handler = lambda cls: isinstance(cls, type) and issubclass(cls, BaseHandler)
+        modules = []
+        handlers=[]
+        def get_file(package):
+            files = os.listdir(package)
+            for file in files:
+                if not file.startswith("__"):
+                    name, ext = os.path.splitext(file)
+                    if ext:
+                        if ext == '.py':
+                            modules.append(package.replace('/', '.') + "." + name)
+                    else:
+                        # 文件夹
+                        get_file(package + "/" + name)
+        get_file(package)
+        for module in modules:
+            module = importlib.import_module(module, package)
+            #遍历模块中的属性
+            for i in dir(module):
+                if i.endswith('Handler') and not i.startswith('Base'):
+                    cls=getattr(module,i)
+                    if is_handler(cls):
+                        handlers.append(cls)
+        return handlers
 
 
     def execute(self,handlers:list):
-        pass
+        for handler in handlers:
+            handler.onHandleMethod(self)
 
 if __name__=='__main__':
     main=Main()
