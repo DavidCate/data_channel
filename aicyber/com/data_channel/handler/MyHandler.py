6c502a4cf2bd4b7666b40f60ce81bbb081a7d5d5
xiong <1783296281@qq.com>
2019-08-07 17:32:40 +0800

    mysql连接成功

--- aicyber/com/data_channel/handler/MyHandler.py
+++ aicyber/com/data_channel/handler/MyHandler.py
@@ -3,24 +3,33 @@ from aicyber.com.data_channel.Task import Task
 
 class MyHandler(BaseHandler):
     async def onHandleMethod(self):
-        task=Task()
-        from_conf=self.getConf()['from']
-        to_conf=self.getConf()['to']
+        # task=Task()
+        # from_conf=self.getConf()['from']
+        # to_conf=self.getConf()['to']
+        #
+        # from_tables=from_conf.get('tables')
+        # to_tables=to_conf.get('tables')
+        #
+        # xx=dict({"cloud_app":['id','addtime']})
+        # for key in xx.keys():
+        #     value=xx.get(key)
+        #
+        # for index in from_tables:
+        #     for key in index.keys():
+        #         value=index.get(key)
+        #     task.setSelectTable(index)
+        #     task.setSelectFields('id','name','age')
+        #     task.setInsertTable(to_tables.pop(index=0))
+        #     task.setInsertFields('id','name','age')
+        #     self.tasks.addTask(task)
+        #
+        # await self.execTasks(self.tasks)
+        sql='select * from student'
+        await self.init()
+        pool=self.getFromPool()
+        async with pool.acquire() as connection:
+            async with connection.cursor() as connection:
+                await connection.execute("select * from student")
+                data = connection.fetchall()
+                print(data)
 
-        from_tables=from_conf.get('tables')
-        to_tables=to_conf.get('tables')
-
-        xx=dict({"cloud_app":['id','addtime']})
-        for key in xx.keys():
-            value=xx.get(key)
-
-        for index in from_tables:
-            for key in index.keys():
-                value=index.get(key)
-            task.setSelectTable(index)
-            task.setSelectFields()
-            task.setInsertTable(to_tables.pop(index=0))
-            task.setInsertFields()
-            self.tasks.addTask(task)
-
-        await self.execTasks(self.tasks)
--- venv/bin/activate.csh
+++ venv/bin/activate.csh
@@ -8,7 +8,7 @@ alias deactivate 'test $?_OLD_VIRTUAL_PATH != 0 && setenv PATH "$_OLD_VIRTUAL_PA
 # Unset irrelevant variables.
 deactivate nondestructive
 
-setenv VIRTUAL_ENV "/home/david/PycharmProjects/data_channel/venv"
+setenv VIRTUAL_ENV "/home/xiong/PycharmProjects/data_channel/venv"
 
 set _OLD_VIRTUAL_PATH="$PATH"
 setenv PATH "$VIRTUAL_ENV/bin:$PATH"
